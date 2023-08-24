cab196c2c34d47cfd7929a7a4de237d684db10ec
Nhat Pham <phamvuonghoangnhat@gmail.com>
2023-08-24 22:33:28 +0700

    First code

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by NhatPham on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
