5881e5b542edb42e062635ba5e4f37cd79b3c6a7
Nhat Pham <phamvuonghoangnhat@gmail.com>
2023-08-25 09:05:36 +0700

    update complete call api

--- SwiftUIAPICalls/ContentView.swift
+++ SwiftUIAPICalls/ContentView.swift
@@ -7,6 +7,46 @@
 
 import SwiftUI
 
+struct URLImage: View{
+    let urlString: String
+    
+    @State var data: Data?
+    
+    
+    var body: some View{
+        if let data = data, let uiimage = UIImage(data: data ){
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130,height: 70)
+                .background(Color.gray)
+            
+        }else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130,height: 70)
+                .background(Color.gray)
+                .onAppear{
+                    fetchData()
+                }
+        }
+    }
+    private func fetchData(){
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data,
+            _, _ in
+            self.data = data
+        }
+        task.resume()
+        
+    }
+}
+
+
+
 struct ContentView: View {
     @StateObject var viewModel = ViewModel();
     
@@ -15,9 +55,8 @@ struct ContentView: View {
             List{
                 ForEach(viewModel.courses, id: \.self){ course in
                     HStack{
-                        Image("")
-                            .frame(width: 130,height: 70)
-                            .background(Color.gray)
+                        URLImage(urlString: course.image)
+                            
                         
                         Text(course.name)
                             .bold()
